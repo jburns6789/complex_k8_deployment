701d3936d1c893b29d9703d8e925ee65ce826295
[joel] <[jbur645@wgu.edu]>
2025-09-16 07:28:10 -0400

    configuring client testing

--- client/Fib.test.js
+++ client/Fib.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import axios from 'axios';
-import Fib from './Fib';
+import Fib from './src/Fib';
 
 jest.mock('axios');
 
